e1b85c6080bb62759153e6a7182f1e738e86e055
Eduardo Lago <eduardolagonunes@gmail.com>
2023-04-20 12:49:12 -0300

    variáveis e funções no react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 export default function App() {
+  const const1=()=>{
+    return ('estudo de js no react')
+  }
+
+  function const2() {
+    return ('texto 2')
+  }
+
   return(
     <>
-      <p>Estudo React</p>
-      <p>Desenvolvendo novas Skills</p>
+      <p>{const1()} {const2()} + um texto qualquer</p>
+      <p>{const2()}</p>
     </>
   )
 }
